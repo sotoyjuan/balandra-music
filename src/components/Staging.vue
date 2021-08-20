29900d0a331781258cc83fcd5c7bbc803cde01a5
Juan Antonio Soto <iregui.js@gmail.com>
2021-08-20 10:44:45 -0500

    Added staging page

---
+++ src/components/Staging.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-center items-center">
+    <iframe
+      src="https://sotoyjuan.github.io/aviario/"
+      height="750"
+      width="1300"
+      frameborder="0"
+    >
+    </iframe>
+    <!-- <img
+      src="https://balandramusic.com/wp-content/uploads/2021/03/Proximamente.png"
+      alt=""
+      class="coming-soon-image"
+    /> -->
+    <div class="desktop-message bg-red-400 text-center p-6 m-1 rounded w-full">
+      <h5 class="text-white font-bold">
+        ¡Lo sentimos! ¡Para disfrutar de Aviario Interactivo debes visitarnos
+        desde un computador o laptop!
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,68 +1,70 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 
 // Components
-import Home from '../components/Home.vue';
-import Post from '../components/Post/Post.vue';
-import Page from '../components/Page/Page.vue';
-import News from '../components/News.vue';
-import Contact from '../components/Contact.vue';
-import Music from '../components/Music.vue';
-import Gallery from '../components/Gallery.vue';
-import Shows from '../components/Shows.vue';
-
-
-
-
+import Home from "../components/Home.vue";
+import Post from "../components/Post/Post.vue";
+import Page from "../components/Page/Page.vue";
+import News from "../components/News.vue";
+import Contact from "../components/Contact.vue";
+import Music from "../components/Music.vue";
+import Gallery from "../components/Gallery.vue";
+import Shows from "../components/Shows.vue";
+import Staging from "../components/Staging.vue";
 
 Vue.use(Router);
 
 const router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: Home,
     },
     {
       // Assuming you're using the default permalink structure for posts
-      path: '/:year/:month/:day/:postSlug',
-      name: 'Post',
+      path: "/:year/:month/:day/:postSlug",
+      name: "Post",
       component: Post,
     },
     {
-      path: '/news',
-      name: 'News',
-      component: News
+      path: "/news",
+      name: "News",
+      component: News,
     },
     {
-      path: '/contact',
-      name: 'Contact',
+      path: "/contact",
+      name: "Contact",
       component: Contact,
     },
     {
-      path: '/music',
-      name: 'Music',
+      path: "/music",
+      name: "Music",
       component: Music,
     },
     {
-      path: '/gallery',
-      name: 'Gallery',
+      path: "/gallery",
+      name: "Gallery",
       component: Gallery,
     },
     {
-      path: '/shows',
-      name: 'Shows',
+      path: "/shows",
+      name: "Shows",
       component: Shows,
     },
     {
-      path: '/:pageSlug',
-      name: 'Page',
+      path: "/testing",
+      name: "Testing",
+      component: Staging,
+    },
+    {
+      path: "/:pageSlug",
+      name: "Page",
       component: Page,
     },
   ],
-  mode: 'history',
-  base: '',
+  mode: "history",
+  base: "",
 
   // Prevents window from scrolling back to top
   // when navigating between components/views
@@ -75,14 +77,13 @@ const router = new Router({
   },
 });
 
-router.afterEach((to) => { // (to, from)
+router.afterEach((to) => {
+  // (to, from)
   // Add a body class specific to the route we're viewing
-  let body = document.querySelector('body');
+  let body = document.querySelector("body");
 
-  const slug = !(to.params.postSlug)
-    ? to.params.pageSlug
-    : to.params.postSlug;
-  body.classList.add('vue--page--' + slug);
+  const slug = !to.params.postSlug ? to.params.pageSlug : to.params.postSlug;
+  body.classList.add("vue--page--" + slug);
 });
 
 export default router;
